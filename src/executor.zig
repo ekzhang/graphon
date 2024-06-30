030e6dbb8ca38a62b2bcf1a79e8145c588ebf763
Eric Zhang <ekzhang1@gmail.com>
2024-06-30 12:29:36 -0400

    Add OperatorState

--- src/executor.zig
+++ src/executor.zig
@@ -1,1 +1,36 @@
 //! Execute query plans against a storage engine.
+
+const std = @import("std");
+const Allocator = std.mem.Allocator;
+
+const Plan = @import("Plan.zig");
+const types = @import("types.zig");
+const Value = types.Value;
+
+/// Execution state attached to a query plan operator while it is running.
+const OperatorState = union(std.meta.Tag(Plan.Operator)) {
+    node_scan,
+    rel_scan,
+    step,
+    begin,
+    repeat,
+    join,
+    semi_join,
+    argument,
+    anti,
+    project,
+    empty_result,
+    filter,
+    limit,
+    distinct,
+    skip,
+    sort,
+    top,
+    union_all,
+    insert_node,
+    insert_edge,
+};
+
+comptime {
+    _ = OperatorState.node_scan;
+}
