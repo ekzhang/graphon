44172c8df07d8505d2c0a2dc29bf53a9882cf174
Eric Zhang <ekzhang1@gmail.com>
2026-06-07 11:07:56 -0700

    Move query tests into query_test

--- src/graphon.zig
+++ src/graphon.zig
@@ -4,6 +4,7 @@ comptime { // Trigger tests to run on these modules.
     _ = @import("executor.zig");
     _ = @import("Plan.zig");
     _ = @import("query.zig");
+    _ = @import("query_test.zig");
     _ = @import("storage.zig");
     _ = @import("tokenizer.zig");
     _ = @import("types.zig");
